c5bd2594d9614b3f9a0eae216bdd13e6723a034f
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-12 14:43:52 +0200

    Aggiunte le classi degli elementi da creare nel db

---
+++ EcommerceContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_ecommerce_db
+{
+    internal class EcommerceContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Order_Product> Orders_Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db_ecommerce;Integrated Security=True");
+        }
+    }
+}
---
+++ Order_Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ecommerce_db
+{
+    internal class Order_Product
+    {
+
+    }
+}
