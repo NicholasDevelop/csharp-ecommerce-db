73b9fe9a888da0b96bf1bb04dd9353b1660b0831
NicholasDevelop <marcaccini.n@gmail.com>
2022-07-13 17:33:01 +0200

    Aggiunta registrazione di un utente e creazione di un ordine in base ai dati dell'utente - milestone 2

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ Console.WriteLine("2- Registrazione utente");
 Console.WriteLine("3- Menu ordini");
 Console.WriteLine("4- Menu recupera lista ordini di un cliente");
 Console.WriteLine("5- Menu modifica ordini di un cliente");
+Console.WriteLine("6- Menu elimina ordine di un cliente");
 Console.WriteLine("0- Esci");
 
 
@@ -41,6 +42,93 @@ switch (menuInput)
 
             db.Add(newProduct);
             db.SaveChanges();
+        }
+        break;
+
+    case 2:
+        Console.WriteLine("**** MENU ISCRIZIONE ****");
+        using (EcommerceContext db = new EcommerceContext())
+        {
+            Console.WriteLine("Inserisci il tuo nome: ");
+            string customerName = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il tuo cognome: ");
+            string customerSurname = Console.ReadLine();
+
+            Console.WriteLine("Insisci la tua email: ");
+            string customerEmail = Console.ReadLine();
+
+            Customer newCustomer = new Customer(customerName, customerSurname, customerEmail);
+
+            db.Add(newCustomer);
+            db.SaveChanges();
+        }
+        break;
+
+    case 3:
+
+        using (EcommerceContext db = new EcommerceContext())
+        {
+            Console.WriteLine("Lista prodotti");
+            List<Product> products = db.Products.OrderBy(product => product.Name).ToList<Product>();
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Id.ToString() + " " + product.Name);
+            }
+            Console.WriteLine("Selezionare cosa si vuole acquistare fino a che non digiti '9' per essere pronto all'acquisto");
+
+            List<Product> productsCart = new List<Product>();
+            List<OrderProduct> quantityProducts = new List<OrderProduct>();
+
+            while (menuInput != 9)
+            {
+                menuInput = Int32.Parse(Console.ReadLine());
+
+                foreach (Product product in products)
+                {
+                    if (menuInput == product.Id)
+                    {
+                        Console.Write("Indicare la quantità:");
+                        menuInput = Int32.Parse(Console.ReadLine());
+                        OrderProduct productQta = new OrderProduct(menuInput, product);
+                        productsCart.Add(product);
+                        quantityProducts.Add(productQta);
+                        Console.WriteLine("Aggiunto");
+                        break;
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("LogIn per chiudere ordine");
+
+            Console.WriteLine("Inserire nome: ");
+            string customerName = Console.ReadLine();
+
+            Console.WriteLine("Inserire email: ");
+            string customerEmail = Console.ReadLine();
+
+            List<Customer> customerList = db.Customers.ToList<Customer>();
+
+            foreach (Customer customer in customerList)
+            {
+                if (customer.Email == customerEmail && customer.CustomerName == customerName)
+                {
+                    Order newOrder = new Order(customer, productsCart);
+                    db.Orders.Add(newOrder);
+                    db.SaveChanges();
+                    foreach (OrderProduct item in quantityProducts)
+                    {
+                        item.OrderID = newOrder.OrderID;
+                        db.OrdersProducts.Add(item);
+                        db.SaveChanges();
+                    }
+
+                }
+
+            }
         }
         break;
     case 0:
